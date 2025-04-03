f5ccf17b2c855016ac622152278333482d19fba6
Stefanochiar <stefanochiardola@gmail.com>
2025-04-03 18:36:24 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+a = input("Ingresá el valor de a: ")
+b = input("Ingresá el valor de b: ")
+
+
+print("Antes del intercambio: a = " + a + ", b = " + b)
+
+aux = a
+a = b
+b = aux
+
+print("Después del intercambio: a = " + a + ", b = " + b)
