4ecd09b15626ae3add399048c1e121a6f65c9d1c
Stefanochiar <stefanochiardola@gmail.com>
2025-04-03 19:28:24 -0300

    Add files via upload

---
+++ untitled2.py
@@ -0,0 +1,15 @@
+frutas = ["manzana","pera","uva","mandarina"]
+frutas.append("banana")
+#print(fruta[1])
+frutas.insert(2, "durazno")
+frutas.remove("uva")
+frutas.sort()
+frutas.reverse()
+for fruta in frutas:
+    print(fruta)
+#while #repite un bloque mientras una condicion se verdadera
+
+contador=0
+while contador < 5:
+    print(contador)
+    contador +=1
